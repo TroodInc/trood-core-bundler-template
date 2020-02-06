df4f39e2bec12e43014cf347760bff89530a0dea
Nileff <krot.odessa.ne@ua.fm>
2020-02-06 14:32:19 +0300

    Tmp storybook

---
+++ src/components/TButton/stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TButton from './index'
+
+
+export default {
+  title: 'TButton',
+  component: TButton,
+}
+
+export const base = () => <TButton label="TButton" />
