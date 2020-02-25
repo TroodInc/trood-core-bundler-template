b387530a6dcd496cdc8d0b30b7fc03041f319474
RFateev <RFateev@MacBook-Pro-Ruslan.local>
2020-02-25 14:22:38 +0300

    TL-1028 separate Table and List

--- src/components/TTable/index.js
+++ src/components/TTable/index.js
@@ -1,19 +1,13 @@
 import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
-import classNames from 'classnames'
 import deepEqual from 'deep-equal'
-
-import style from './index.css'
-import { messages } from './constants'
-
-import { intlObject } from '$trood/localeService'
 
 import { AppContext } from '$trood/app'
 
 import { isDefAndNotNull } from '$trood/helpers/def'
-import { isReactComponent } from '$trood/helpers/react'
-import TCheckbox, { CHECK_COLORS } from '$trood/components/TCheckbox'
-import TIcon, { ICONS_TYPES, ROTATE_TYPES } from '$trood/components/TIcon'
+
+import TableView from './components/TableView'
+import ListView from './components/ListView'
 
 
 const equal = (newItem, oldArray) => {
@@ -163,205 +157,58 @@ class TTable extends PureComponent {
       <AppContext.Consumer>
         {({ media }) => {
           if (media.portable) {
-            const headerItemsSortable = headerFiltered.filter(item => item.sortable)
-
             return (
-              <div className={style.tableList}>
-                <div className={style.tableListHeader}>
-                  {(checking || listTitle) && (
-                    <div className={style.tableListTitleContainer}>
-                      {checking &&
-                        <TCheckbox {...{
-                          className: style.listCheckbox,
-                          value: this.getCheckedCount() === check.length,
-                          onChange: this.checkAll,
-                          color: CHECK_COLORS.orange,
-                        }} />
-                      }
-                      {checking && !listTitle && (
-                        <span className={style.tableListTitle}>{intlObject.intl.formatMessage(messages.checkAll)}</span>
-                      )}
-                      {listTitle && <span className={style.tableListTitle}>{listTitle}</span>}
-                    </div>
-                  )}
-                  {body.length > 0 && headerItemsSortable.length > 0 && (
-                    <div className={style.listSortControls}>
-                      <span className={style.listSortControlsLabel}>
-                        {intlObject.intl.formatMessage(messages.sortBy)}
-                      </span>
-                      {headerItemsSortable.map((item, index) => (
-                        <div {...{
-                          key: index,
-                          onClick: () => onSort(item.name, sortingOrder === -1 ? 1 : -1),
-                          className: style.listSortItem,
-                        }}>
-                          {item.title}
-                          {sortingColumn === item.name &&
-                            <TIcon {...{
-                              type: ICONS_TYPES.arrow,
-                              rotate: sortingOrder === -1 ? ROTATE_TYPES.up : ROTATE_TYPES.down,
-                              className: style.sortIcon,
-                              size: 16,
-                            }} />
-                          }
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div>
-                {body.map((row, r) => {
-                  let currentMetaData
-                  if (modelMetaData) {
-                    currentMetaData = modelMetaData(row, r, body)
-                  }
-                  const headerModel = typeof listHeaderModel === 'function'
-                    ? listHeaderModel(row, r, body, currentMetaData)
-                    : headerFiltered[0] && headerFiltered[0].model(row, r, body, currentMetaData)
-                  const expanded = listItemExpanded === r
+              <ListView {...{
+                className,
 
-                  return (
-                    <div {...{
-                      key: this.getRowKey(row, r, body),
-                      className: classNames(
-                        style.tableListItemContainer,
-                        expanded && style.expanded,
-                        row.rowClassName,
-                        typeof rowClassName === 'function' ? rowClassName(row, r, body, currentMetaData) : rowClassName,
-                      ),
-                    }}>
-                      <div {...{
-                        className: classNames(style.tableListItemHeader, expanded && style.expanded),
-                        onClick: () => this.toggleExpand(r),
-                      }}>
-                        <div className={style.tableListItemTitle}>
-                          {checking && (
-                            <TCheckbox {...{
-                              className: style.listCheckbox,
-                              value: check[r],
-                              onChange: () => this.checkItem(r),
-                              stopPropagation: true,
-                              color: CHECK_COLORS.orange,
-                            }} />
-                          )}
-                          <div className={classNames(style.tableListItemTitleText, expanded && style.expanded)}>
-                            {isReactComponent(headerModel)
-                              ? React.cloneElement(headerModel, { onClick: undefined })
-                              : headerModel
-                            }
-                          </div>
-                        </div>
-                        <TIcon {...{
-                          type: ICONS_TYPES.arrow,
-                          rotate: expanded ? 180 : 0,
-                          size: 24,
-                          className: style.expandIcon,
-                        }} />
-                      </div>
-                      {expanded && (
-                        <div className={classNames(
-                          style.tableListItemBody,
-                          checking && style.tableListItemBodyChecking,
-                        )}>
-                          {headerFiltered.map((item, i) => (
-                            <div {...{
-                              key: i,
-                              className: classNames(item.className, style.listItemBodyRow),
-                            }}>
-                              {item.title && <span className={style.tableListItemLabel}>{item.title}:</span>}
-                              <div className={style.tableListItemValue}>
-                                {item.model(row, r, body, currentMetaData)}
-                              </div>
-                            </div>
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                  )
-                })}
-              </div>
+                listTitle,
+                listHeaderModel,
+
+                checking,
+                check,
+                checkAll: this.checkAll,
+                checkItem: this.checkItem,
+                getCheckedCount: this.getCheckedCount,
+
+                body,
+                header: headerFiltered,
+
+                onSort,
+                sortingOrder,
+                sortingColumn,
+
+                modelMetaData,
+                listItemExpanded,
+                toggleExpand: this.toggleExpand,
+                getRowKey: this.getRowKey,
+                rowClassName,
+              }} />
             )
           }
 
           return (
-            <table className={classNames(style.table, className)}>
-              <thead>
-              <tr className={headerClassName}>
-                {checking &&
-                  <th className={style.checkCell}>
-                    <TCheckbox {...{
-                      value: this.getCheckedCount() === check.length,
-                      onChange: this.checkAll,
-                      color: CHECK_COLORS.orange,
-                    }} />
-                  </th>
-                }
-                {header.filter(item => item.show !== false).map((item, i) => {
-                  return (
-                    <th key={i} className={item.className}>
-                      <div {...{
-                        className: item.sortable ? style.headerWrapperSortable : style.headerWrapper,
-                        'data-cy': item.title,
-                        onClick: () => {
-                          if (item.sortable) {
-                            onSort(item.name, sortingOrder === -1 ? 1 : -1)
-                          }
-                        },
-                      }}>
-                        {item.title}
-                        {item.sortable && sortingColumn === item.name &&
-                          <TIcon {...{
-                            type: ICONS_TYPES.arrow,
-                            rotate: sortingOrder === -1 ? ROTATE_TYPES.up : ROTATE_TYPES.down,
-                            className: style.sortIcon,
-                            size: 16,
-                          }} />
-                        }
-                      </div>
-                    </th>
-                  )
-                })}
-              </tr>
-              </thead>
-              <tbody className={style.tbody}>
-              {body.map((row, r) => {
-                let currentMetaData
-                if (modelMetaData) {
-                  currentMetaData = modelMetaData(row, r, body)
-                }
-                return (
-                  <tr {...{
-                    onClick: () => onRowClick(row, r, body),
-                    key: this.getRowKey(row, r, body),
-                    className: classNames(
-                      row.rowClassName,
-                      typeof rowClassName === 'function' ? rowClassName(row, r, body, currentMetaData) : rowClassName,
-                    ),
-                  }}>
-                    {checking &&
-                      <td className={style.checkCell} data-cy={`table_cell_${r}_checkbox`}>
-                        <TCheckbox {...{
-                          value: check[r],
-                          onChange: () => this.checkItem(r),
-                          stopPropagation: true,
-                          color: CHECK_COLORS.orange,
-                        }} />
-                      </td>
-                    }
-                    {headerFiltered.map((item, i) => (
-                      <td {...{
-                        'data-cy': `table_cell_${r}_${i}`,
-                        key: i,
-                        className: item.className,
-                        colSpan: item.colSpan && item.colSpan(row, r, body),
-                      }}>
-                        {item.model(row, r, body, currentMetaData)}
-                      </td>
-                    ))}
-                  </tr>
-                )
-              })}
-              </tbody>
-            </table>
+            <TableView {...{
+              className,
+              headerClassName,
+
+              checking,
+              check,
+              getCheckedCount: this.getCheckedCount,
+              checkAll: this.checkAll,
+              checkItem: this.checkItem,
+
+              header: headerFiltered,
+              body,
+
+              onSort,
+              sortingOrder,
+              sortingColumn,
+
+              modelMetaData,
+              onRowClick,
+              getRowKey: this.getRowKey,
+              rowClassName,
+            }} />
           )
         }}
       </AppContext.Consumer>
