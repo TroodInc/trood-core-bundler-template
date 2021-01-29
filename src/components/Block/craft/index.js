5cf8d63a462bfe015ad8fc194c6fd8e8cd438697
Ruslan Fateev <rufateev@gmail.com>
2021-01-29 17:25:33 +0300

    TB-714 (#90)
    * TB-714
    
    * TB-714
    
    * fixes
    
    Co-authored-by: RFateev <RFateev@MacBook-Pro-Ruslan.local>

---
+++ src/components/Block/craft/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useNode } from '@craftjs/core'
+
+import Settings from './Settings'
+import Block from '../index'
+
+
+const CraftBlock = props => {
+  const {
+    connectors: { connect, drag },
+  } = useNode((node) => ({ props: node.data.props }))
+
+  return (
+    <div ref={ref => connect(drag(ref))}>
+      <Block {...props}>
+        {props.children}
+      </Block>
+    </div>
+  )
+}
+
+CraftBlock.craft = {
+  displayName: 'Block',
+  related: {
+    settings: Settings,
+  },
+  rules: {
+    canDrag: () => true,
+    canMoveIn: () => true,
+  },
+}
+
+export default CraftBlock
